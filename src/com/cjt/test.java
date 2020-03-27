216a496f2cbc04541641cc42f209b470024603bc
TAOJIANGCAI <826907729@qq.com>
2020-03-27 17:04:53 +0800

    把本地工程添加到git

---
+++ src/com/cjt/test.java
@@ -0,0 +1,18 @@
+package com.cjt;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: cjt
+ * @Date: 2020/03/27/16:55
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+    }
+}
